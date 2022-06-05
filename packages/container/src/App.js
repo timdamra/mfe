daa8933ed5d216a0b9bf864618c35e83f1cedc04
Hatim Damra <hatim.damra@teal-c02dw6zfmd6p.attlocal.net>
2022-06-05 16:17:10 -0500

    fixed css naming conflicts

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
+// createGenerateClassName is useful for avoiding css naming conflicts
+// can be caused by the css class name generating process
+const generateClassName = createGenerateClassName({
+    // this option specifies a prefix to add to generated class names
+    productionPrefix: 'co'
+});
+
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     )
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+// createGenerateClassName is useful for avoiding css naming conflicts
+// can be caused by the css class name generating process
+const generateClassName = createGenerateClassName({
+    // this option specifies a prefix to add to generated class names
+    productionPrefix: 'ma'
+});
+
 export default () => {
-    
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                   <Switch>
                       <Route exact path="/pricing" component={Pricing} />
